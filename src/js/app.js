b608a060ce3cd927a5cdb640255e716edf4fea85
yorkHan <393900482@qq.com>
2018-05-11 22:28:07 +0800

    实现登录注册，和登录后保存个人信息的功能

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ let app=new Vue({
     el:'#page',
     data:{
         loginVisible:false,
-        signUpVisible:false,
+        signUpVisible:true,
         resume:{
             name:'姓名',
             job:'应聘岗位',
@@ -10,29 +10,61 @@ let app=new Vue({
             birthday:'19930916',
             phoneNumber:'15444455588',
             email:'393900482@qq.com'
+        },
+        logIn:{
+            username:'',
+            password:''
+        },
+        signUp:{
+            email:'',
+            username:'',
+            password:''
         }
     },
     methods:{
         onEdit(key,value){
             this.resume[key]=value
         },
-        onclickSave(){
+        onClickSave(){
             let currentUser = AV.User.current();
-            if(!currentUser){
-                this.loginVisible=true
-            }else{
+            if(currentUser){
                 this.saveResume()
+            }else{
+                this.loginVisible=true
             }
         },
-        saveResume(){
-            let User = AV.Object.extend('User');
-            let user = new User();
-            user.set('resume',this.resume);
-            user.save().then(function (todo) {
-
+        onSignUp(){
+            let user = new AV.User();
+            // 设置用户名
+            user.setUsername(this.signUp.username);
+            // 设置密码
+            user.setPassword(this.signUp.password);
+            // 设置邮箱
+            user.setEmail(this.signUp.email);
+            user.signUp().then(function (user) {
+                console.log(user);
             }, function (error) {
-                console.error(error);
             });
+        },
+        onLogIn(){
+            AV.User.logIn(this.logIn.username,this.logIn.password).then(function (user) {
+                console.log(user);
+            }, function (error) {
+                if(error.code===211){
+                    alert('用户名不存在')
+                }else if(error.code===210){
+                    alert('密码错误')
+                }
+            });
+        },
+        saveResume(){
+            console.log(AV.User.current());
+            let id=AV.User.current().id
+            let user = AV.Object.createWithoutData('User',id);
+            // 修改属性
+            user.set('resume',this.resume);
+            // 保存到云端
+            user.save();
         }
     }
 })
